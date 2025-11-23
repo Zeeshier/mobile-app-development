dbf9d0e8cd797546aa034856728de731318c3356
Zeeshan Ahmad <Zeeshanwarraich51@gmail.com>
2025-11-23 16:13:58 +0500

    control flow

---
+++ 1_Dart Programming Fundamentals/02_Control_Flow/01_if_else.dart
@@ -0,0 +1,13 @@
+void main() {
+  int a = 100;
+
+  if (a > 200) {
+    print("Block 1 executed");
+  } else if (a > 50) {
+    print("Block 2 executed");
+  } else if (a > 10) {
+    print("Block 3 executed");
+  } else {
+    print("Block 4 executed");
+  }
+}
---
+++ 1_Dart Programming Fundamentals/02_Control_Flow/02_ternary_operator.dart
@@ -0,0 +1,9 @@
+void main() {
+  int a = 100;
+  
+  // Shorthand for if-else
+  // condition ? expression_if_true : expression_if_false
+  String result = (a > 50) ? "A is large" : "A is small";
+  
+  print(result);
+}
